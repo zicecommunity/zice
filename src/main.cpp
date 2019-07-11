8e3607272a4574db81cea3eb12366fe3e941cb1b
skellers21 <iain21lawrie@gmail.com>
2019-07-11 23:53:15 +0100

    block subsidy to 5

--- src/main.cpp
+++ src/main.cpp
@@ -1757,7 +1757,7 @@ bool ReadBlockFromDisk(CBlock& block, const CBlockIndex* pindex, const Consensus
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    CAmount nSubsidy = 12.5 * COIN;
+    CAmount nSubsidy = 5 * COIN;
 
     // Mining slow start
     // The subsidy is ramped up linearly, skipping the middle payout of
